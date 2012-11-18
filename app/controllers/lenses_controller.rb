1fc0a87d300582e7555054c32709b4f0883b8c3e
Carsten Senkfeil <noheadlights@me.com>
2012-11-18 16:22:19 +0100

    flickr

--- app/controllers/lenses_controller.rb
+++ app/controllers/lenses_controller.rb
@@ -36,6 +36,9 @@ class LensesController < ApplicationController
       format.json { render json: @lense }
     end
   end
+  def index
+    @photos = get_flickr_pictures()
+  end
 
   # GET /lenses/1/edit
   def edit
@@ -85,4 +88,24 @@ class LensesController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  private
+    def get_flickr_pictures
+      FlickRaw.api_key="8fa76ef7873d2eac9c08e7773838a19c"
+      FlickRaw.shared_secret="0d534faad9611d07"
+      @photos = {}
+      flickr.photos.search(:user_id => "26276803@N05", :tags => 'carousel').each do |p|
+        info = flickr.photos.getInfo(:photo_id => p.id) # retrieve additional details
+                                                        # p info # uncomment to see other details
+        title = info.title
+        square_url = FlickRaw.url_s(info)
+        original_url = FlickRaw.url_o(info)
+        taken = info.dates.taken
+        views = info.views
+        tags = info.tags.map {|t| t.raw}
+        @photos[title]= FlickRaw.url_b(info)
+      end
+      return @photos
+    end
+
 end
